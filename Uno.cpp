1912e2f00369788a8a020b4de69d3a7a3334a49d
Sasho <i.aleks.kostadinov@gmail.com>
2025-12-22 15:37:46 +0200

    Fisrt try for array creation
    Trying things for the array

--- Uno.cpp
+++ Uno.cpp
@@ -4,5 +4,21 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+
+    char cha[2][4] = {
+  { 'h', 'f', 'v', 'g'},
+  { 'a', 's', 'd', 'g'}
+    };
+    for (size_t i = 0; i < 4; i++)
+    {
+        for (size_t j = 0; j < 2; j++)
+        {
+            std::cout << cha[j][i];
+        }
+    }
+    
+
+    return 0;
 }
